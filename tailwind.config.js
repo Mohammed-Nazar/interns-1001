4c60da9e606980f85d3dd4ffe49203569d5bdd54
elaf <elafghassangardi@gmail.com>
2024-08-21 12:19:23 +0300

    feat(color): add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,14 @@ module.exports = {
       colors:{
         NavyN900: "#000119",
         Green: "#13CB5C",
-        NavyN600: "#2E2F42"
+        NavyN600: "#2E2F42",
+        NavyN70: "#A3A4AC"
       },
       spacing: {
         '26px': '26.06px',
-        '29.9px': '29.9px',
-        '36.4px': '36.4px',
-        '120px': '120px'
+        '538px': '538.06px',
+        '346px':'346.06px',
+        '154px': '154.06px'
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
