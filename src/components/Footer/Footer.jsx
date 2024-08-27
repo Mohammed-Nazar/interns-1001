faa1e0b0fec56c1c8788c22f1a8a71cfc408d19f
Mohammed Nazar <x3raqe@gmail.com>
2024-08-27 12:36:01 +0300

    styel(footer): add margin to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row md:justify-between md:grid-cols-2 pb-5 mb-5 border-b-[2px] border-navy-800">
         <div className="text-navy-70 flex flex-col gap-6 col-span-1 w-fit">
           <Image src="1001/LogoGreen.svg" width={60} height={60} />
-          <div className="flex flex-col w-fit">
+          <div className="flex flex-col w-fit mb-2">
             <span>support@1001.tv</span>
             <span dir="ltr">+964(780)133-3323</span>
           </div>
@@ -23,17 +23,17 @@ const Footer = () => {
             <li className="cursor-pointer"><FaYoutube/></li>
           </ul>
         </div>
-        <div className="my-5 flex gap-10 md:gap-20">
+        <div className="my-5 flex flex-col gap-2 md:flex-row md:gap-20">
           <div>
             <h4 className="font-ar-600 text-navy-50">بدء الاستخدام</h4>
             <ul>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 كيف يعمل 1001 ؟
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 الحساب
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 استرداد كوبون
               </li>
             </ul>
@@ -41,13 +41,13 @@ const Footer = () => {
           <div>
             <h4 className="font-ar-600 text-navy-50">الشركة</h4>
             <ul>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 من نحن ؟
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 الوظائف
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 الصحافة
               </li>
             </ul>
@@ -55,13 +55,13 @@ const Footer = () => {
           <div>
             <h4 className="font-ar-600 text-navy-50">الدعم</h4>
             <ul>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 الأسئلة الشائعة
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 مركز الدعم
               </li>
-              <li className="font-ar-400 text-navy-70 cursor-pointer my-1">
+              <li className="font-ar-400 text-navy-70 cursor-pointer my-3">
                 اتصل بنا
               </li>
             </ul>
