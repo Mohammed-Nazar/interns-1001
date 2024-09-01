d6804f676fc21d958d4bece34f2185f42f88a540
Mohammed Nazar <x3raqe@gmail.com>
2024-09-01 15:08:17 +0300

    style(ui): add tailwind colors and BGs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,31 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        "subscription-gradient": "url('/BG.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        primary: "#13CB5C",
+        "navy-600": "#2E2F42",
+        "navy-900": "#000119",
+        "navy-700": "#1C1D32",
+      },
+      colors: {
+        "navy-400": "#4A4B5C",
+        primary: "#13CB5C",
+        "navy-70": "#bfc0c6",
+        "navy-900": "#000119",
+        "navy-800": "#0D0E25",
+        "navy-50": "#BFC0C6",
+        "navy-100": "#757683",
+      },
+      borderColor: {
+        "navy-700": "#1C1D32",
+      },
+      borderColor: {
+        "navy-300": "#575767",
       },
     },
   },
