ee1b23cf689e42e8d43abac0a2003d5b4457d4ed
Mohammed Nazar <x3raqe@gmail.com>
2024-09-11 02:47:31 +0300

    fix: home page data (get data from json file)

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,16 +2,41 @@ import Link from "next/link"
 import React from "react"
 import Carousel from "../Carousel/Carousel"
 
-const HomePage = ({
-  popularMoviesData,
-  nowPlayingMoviesData,
-  topRatedMoviesData,
-  topRatedTvShowsData,
-  trendingNowMoviesData,
-}) => {
-  const isFree = false
+const titleMapping = {
+  "Web Main slider": "Web Main Slider",
+  "أعمال 1001 الأصلية - متوفرة الآن": "1001 Originals - Available Now",
+  "الاكثر مشاهدة": "Most Watched",
+  أنميشين: "Animation",
+  "دراما أجنبية": "Foreign Drama",
+  "دراما آسيوية": "Asian Drama",
+  "دراما عراقية": "Iraqi Drama",
+  "دراما كلاسيكية": "Classic Drama",
+  "دراما عربية": "Arab Drama",
+  "برامج منوعة": "Diverse Shows",
+  برامج: "Shows",
+  "برامج كوميدية": "Comedy Shows",
+  "مسلسلات غنائية": "Musical Series",
+  "مسلسلات الجريمة": "Crime Series",
+  وثائقيات: "Documentaries",
+  رومانسي: "Romantic",
+  "أعمال 1001 الأصلية": "1001 Originals",
+  "مسلسلات محدودة": "Watch For Free",
+  "اخترنا لك": "Recommended for you",
+}
+
+const HomePage = ({ mediaContent }) => {
+  // Extract unique section titles, excluding "Web Main slider"
+  const sectionTitles = Array.from(
+    new Set(
+      mediaContent
+        .map((item) => item.sectionTitle)
+        .filter((title) => title !== "Web Main slider"),
+    ),
+  )
+
   return (
     <main>
+      {/* Hero section */}
       <section className="home-section-bg h-72 md:h-5/6 p-5 flex flex-row items-center justify-between">
         <div className="flex flex-col justify-center w-60 md:w-96">
           <h1 className="text-primary text-sm font-inter font-bold mb-4 md:text-4xl">
@@ -19,38 +44,30 @@ const HomePage = ({
           </h1>
           <Link href="/1001">
             <button className="bg-primary px-4 py-3 rounded-xl text-sm font-inter font-semibold text-white md:text-2xl">
-                Browse all
+              Browse all
             </button>
           </Link>
         </div>
         <div className="w-64 md:w-8/12">
-          <Carousel isHero={true} data={popularMoviesData} />
-        </div>
-      </section>
-      
-      <section className="bg-navy-900 pl-5">
-        <div className="py-2">
-          <Carousel title="Recently added" data={nowPlayingMoviesData} />
-        </div>
-        <div className="py-2">
-          <Carousel title="Movies you Must Watch" data={topRatedMoviesData} />
-        </div>
-        <div className="py-2">
-          <Carousel title="Series You Must Watch" data={topRatedTvShowsData} />
-        </div>
-        <div className="py-2">
-          <Carousel title="Most Watched" data={trendingNowMoviesData} />
-        </div>
-        <div className="py-2">
-          <Carousel title="Best of OSN+" data={topRatedMoviesData} />
-        </div>
-        <div className="py-2">
           <Carousel
-            isFree={true}
-            title="Watch For Free"
-            data={nowPlayingMoviesData}
+            isHero={true}
+            data={mediaContent.filter(
+              (i) => i.sectionTitle === "Web Main slider",
+            )}
           />
         </div>
+      </section>
+
+      {/* Other carousels */}
+      <section className="bg-navy-900 pl-5">
+        {sectionTitles.map((title) => (
+          <div className="py-2" key={title}>
+            <Carousel
+              title={titleMapping[title] || title}
+              data={mediaContent.filter((i) => i.sectionTitle === title)}
+            />
+          </div>
+        ))}
       </section>
     </main>
   )
