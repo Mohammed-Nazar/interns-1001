0960d88b968bc814348f36332cccea8794006f9e
elaf <elafghassangardi@gmail.com>
2024-09-10 08:59:22 +0300

    feat: add mokeUpUser to be able to access the user info globally

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,10 +1,16 @@
 "use client";
 import { useState, createContext } from "react"
 
+const mokeUpUser = {
+  id: null,
+  phoneNumber: "",
+  required_package: null,
+}
+
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(mokeUpUser)
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
