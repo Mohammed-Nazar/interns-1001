45e7f3e46a8c164f04415467b82721db95b18278
elaf <elafghassangardi@gmail.com>
2024-09-10 04:01:23 +0300

    fix: increase component margin (mx-3 to mx-9) for md screens

--- src/components/subscription agreement notice/SubscriptionAgreementNotice.jsx
+++ src/components/subscription agreement notice/SubscriptionAgreementNotice.jsx
@@ -1,24 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link"
+import React from "react"
 
 const SubscriptionAgreementNotice = () => {
   return (
-    <div className="mx-0 md:mx-3">
-        <p className="mt-4 text-sm font-normal leading-6 tracking-wide text-NavyN50 text-start">
-          By Subscribing, you accept the following 1001's{" "}
-          <Link href="/" className="text-Green">
-            Privacy Policy
-          </Link>{" "}
-          &{" "}
-          <Link href="/" className="text-Green">
-            Terms of Service.
-          </Link>{" "}
-          If you need any assistance, please{" "}
-          <Link href="/" className="text-Green">
-            contact our support team
-          </Link>
-          .
-        </p>
+    <div className="mx-0 md:mx-9">
+      <p className="mt-4 text-sm font-normal leading-6 tracking-wide text-NavyN50 text-start">
+        By Subscribing, you accept the following 1001's{" "}
+        <Link href="/" className="text-Green">
+          Privacy Policy
+        </Link>
+        &
+        <Link href="/" className="text-Green">
+          Terms of Service.
+        </Link>
+        If you need any assistance, please{" "}
+        <Link href="/" className="text-Green">
+          contact our support team
+        </Link>
+        .
+      </p>
     </div>
   )
 }
