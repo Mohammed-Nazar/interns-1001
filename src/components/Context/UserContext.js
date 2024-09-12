e9af9eebc95165884796abc133497895be5f651d
Mohammed Nazar <x3raqe@gmail.com>
2024-09-12 22:53:55 +0300

    fix: fix user context provider

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,10 +1,26 @@
-"use client";
-import { useState, createContext } from "react"
+"use client"
+import { useState, useEffect, createContext } from "react"
 
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(null)
+
+  // Fetch user data from localStorage when the app loads
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user")
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user))
+    } else {
+      localStorage.removeItem("user")
+    }
+  }, [user])
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
